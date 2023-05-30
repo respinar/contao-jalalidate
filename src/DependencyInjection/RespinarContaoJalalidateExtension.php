6ee6ddcffbe1f22bddd1023d99ea551dc073c407
Hamid Paywasti <abbaszadeh.h@gmial.com>
2023-05-30 18:26:05 +0330

    Convert Extenstion to Bundle

---
+++ src/DependencyInjection/RespinarContaoJalalidateExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Jalali Date Bundle.
+ *
+ * (c) Hamid Paywasti 2023 <abbaszadeh.h@gmail.com>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-jalalidate
+ */
+
+namespace Respinar\ContaoJalalidate\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class RespinarContaoJalalidateExtension extends Extension
+{
+    /**
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../config')
+        );
+        
+    }
+}
