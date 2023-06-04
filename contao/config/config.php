5541efa34e62d5c87399a6222a67ee42a9d5b2e2
Hamid Paywasti <abbaszadeh.h@gmail.com>
2023-06-04 21:55:16 +0330

    Add new config file

---
+++ contao/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of Contao Jalali Date Bundle.
+ *
+ * (c) Hamid Paywasti 2023 <abbaszadeh.h@gmail.com>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-jalalidate
+ */
+
+use Respinar\ContaoJalalidate\EventListener\ParseDateListener;
+
+$GLOBALS['TL_HOOKS']['parseDate'][] = [ParseDateListener::class, '__invoke'];
