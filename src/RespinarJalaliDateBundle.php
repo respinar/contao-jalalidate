cf45ece74491fcfa83632945eef3e95c20572a77
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-16 11:41:50 +0330

    Rename  Paywasti to Peywasti

--- src/RespinarJalaliDateBundle.php
+++ src/RespinarJalaliDateBundle.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Contao Jalali Date Bundle.
  *
- * (c) Hamid Paywasti 2023 <abbaszadeh.h@gmail.com>
+ * (c) Hamid Peywasti 2023 <abbaszadeh.h@gmail.com>
  * @license MIT
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
