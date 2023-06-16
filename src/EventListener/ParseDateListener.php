dc14babefa690a9a1bba954a3a2c101bb487253d
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-16 11:26:40 +0330

    Change to Attribute

--- src/EventListener/ParseDateListener.php
+++ src/EventListener/ParseDateListener.php
@@ -2,16 +2,31 @@
 // src/EventListener/ParseDateListener.php
 namespace Respinar\JalaliDateBundle\EventListener;
 
-use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;   
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Respinar\JalaliDateBundle\Helper\jDateTime;
 
 /**
  * @Hook("parseDate")
  */
+#[AsHook('parseDate')]
 class ParseDateListener
 {
+    public function __construct()
+    {
+    }
+
     public function __invoke(string $formattedDate, string $format, ?int $timestamp): string
     {
+        //$request = $this->requestStack->getCurrentRequest();
+
+       // \var_dump($request);
+
+        // if (!$request || !$this->scopeMatcher->isBackendRequest($request)) {
+        //     return;
+        // }
+
         if ($GLOBALS['TL_LANGUAGE'] !== 'fa') {
             return $formattedDate;
         }
