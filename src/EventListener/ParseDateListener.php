bdd9b4b0ecac8d49c3fce6e3ac912332328b1c8f
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-16 11:35:10 +0330

    remove __construct()

--- src/EventListener/ParseDateListener.php
+++ src/EventListener/ParseDateListener.php
@@ -2,28 +2,24 @@
 // src/EventListener/ParseDateListener.php
 namespace Respinar\JalaliDateBundle\EventListener;
 
-use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;   
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Respinar\JalaliDateBundle\Helper\jDateTime;
 
 #[AsHook('parseDate')]
 class ParseDateListener
 {
-    public function __construct()
-    {
-    }
-
     public function __invoke(string $formattedDate, string $format, ?int $timestamp): string
     {
         if ($GLOBALS['TL_LANGUAGE'] !== 'fa') {
             return $formattedDate;
         }
-        
+
         if ($timestamp === null) {
             $strDate = jDateTime::date($format);
         } else {
             $strDate = jDateTime::date($format, $timestamp);
-        }	
+        }
 
-		return $strDate;        
+		return $strDate;
     }
 }
