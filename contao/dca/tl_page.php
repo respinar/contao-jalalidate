c881cf1321c6152a60484b374512a51b08fb95ba
Hamid Peywasti <hamid@respinar.com>
2024-03-26 20:22:31 +0330

    rename to use IranianDate

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -12,16 +12,16 @@ declare(strict_types=1);
 
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-// Add the 'useShamsiDate' field to the 'root' and 'rootfallback' palettes after 'global_legend'
+// Add the 'useIranianDate' field to the 'root' and 'rootfallback' palettes after 'global_legend'
 PaletteManipulator::create()
-    ->addLegend('shamsi_legend', 'global_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('useShamsiDate', 'shamsi_legend', PaletteManipulator::POSITION_APPEND)
+    ->addLegend('iranian_date_legend', 'global_legend', PaletteManipulator::POSITION_AFTER)
+    ->addField('useIranianDate', 'iranian_date_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('root', 'tl_page')
     ->applyToPalette('rootfallback', 'tl_page');
 
 // Define the field
-$GLOBALS['TL_DCA']['tl_page']['fields']['useShamsiDate'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_page']['useShamsiDate'],
+$GLOBALS['TL_DCA']['tl_page']['fields']['useIranianDate'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_page']['useIranianDate'],
     'exclude'   => true,
     'inputType' => 'checkbox',
     'eval'      => ['tl_class' => 'w50 clr'],
