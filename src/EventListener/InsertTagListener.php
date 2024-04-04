ff027fb13918c0ac5e339edc70e69becf7b4d9fd
Hamid Peywasti <hamid@respinar.com>
2024-04-04 11:27:35 +0330

    add iranian_date InsertTag

---
+++ src/EventListener/InsertTagListener.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Jalali Date Bundle.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\JalaliDateBundle\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Respinar\JalaliDateBundle\Helper\jDateTime;
+
+#[AsHook('replaceInsertTags')]
+class InsertTagListener
+{
+    public function __invoke(string $tag): string|false
+    {
+        // Split the tag into parts (e.g., "iranian_date::j F Y" for custom format)
+        $parts = explode('::', $tag);
+
+        // Check if the tag is "iranian_date"
+        if ($parts[0] !== 'iranian_date') {
+            return false; // Let Contao handle other tags
+        }
+
+        // Get the format: If not provided, use global settings
+        $format = $parts[1] ?? $GLOBALS['objPage']->dateFormat ?? $GLOBALS['TL_CONFIG']['dateFormat'] ?? '';
+
+        // Get the timestamp, if provided
+        $timestamp = $parts[2] ?? time();
+
+        // Convert timestamp from string to integer (if it's a number)
+        if (is_numeric($timestamp)) {
+            $timestamp = (int) $timestamp;
+        }
+
+        return jDateTime::date($format, $timestamp);
+    }
+}
